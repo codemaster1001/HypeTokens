8d94adec648442da8c7c11c63025e7d68153abd3
codemaster1001 <blockchaincrazy1001@gmail.com>
2024-06-24 05:57:42 +0200

    finalize

--- ignition/modules/HypeNFTV2.ts
+++ ignition/modules/HypeNFTV2.ts
@@ -4,7 +4,7 @@ import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
  * @notice Before deploying you have to set the correct HypeCoin address as a parameter in contructor function
  */
 const HypeNFTV2Module = buildModule("HypeNFTV2Module", (m) => {
-  const HypeCoinAddress = "0x142dd3c2d1fDDF77216c5AA7eAde4FdCf072efA9"; // This is a temparary address of deployed HypeCoin Address.
+  const HypeCoinAddress = "0xE936844Dfe217f79A5F8f1E5D29f5c3AEb809116"; // This is a temparary address of deployed HypeCoin Address.
   const nftV2 = m.contract("HypeNFTV2", [HypeCoinAddress]);
   return { nftV2 };
 });
